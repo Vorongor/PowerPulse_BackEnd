1059851e643de184797018a99147e270bcd59eaf
Vorongor <mr.maddarknes@gmail.com>
2023-11-11 10:18:09 +0200

    fixes

--- routes/statisticRouters.js
+++ routes/statisticRouters.js
@@ -1,22 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { passportAuthenticate } = require("./midleware/auth");
 const statisticController = require("./controlers/statisticController");
 
-const checkToken = (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
-  if (!token) {
-    throw HttpError(401, "Unauthorized");
-  }
-  next();
-};
-
-router.use(checkToken);
-
-router.get("/video", passportAuthenticate, statisticController.getVideo);
-router.get("/calories", passportAuthenticate, statisticController.getCalories);
-router.get("/users", passportAuthenticate, statisticController.getUsers);
-router.get("/time", passportAuthenticate, statisticController.getTime);
-router.get("/training", passportAuthenticate, statisticController.getTraining);
+router.get("/video", statisticController.getVideo);
+router.get("/calories", statisticController.getCalories);
+router.get("/users", statisticController.getUsers);
+router.get("/time", statisticController.getTime);
+router.get("/training", statisticController.getTraining);
 
 module.exports = router;
