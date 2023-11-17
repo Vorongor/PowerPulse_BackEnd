33a63b924eb6afa5b26e8a5b40259141e9f594f6
Vorongor <mr.maddarknes@gmail.com>
2023-11-17 14:08:11 +0200

    stat rew

--- routes/controlers/statisticController.js
+++ routes/controlers/statisticController.js
@@ -3,98 +3,109 @@ const { User } = require("../../db/usersSchema");
 const { Exercise } = require("../../db/exerciseSchema");
 const { ExerciseLog } = require("../../db/exercisesLogSchema");
 
-const getVideo = async (req, res, next) => {
+const getStatistic = async (req, res, next) => {
   try {
-    const result = await Exercise.countDocuments({
+    const videos = await Exercise.countDocuments({
       gifUrl: { $exists: true, $ne: null },
     });
-    if (!result) {
-      throw HttpError(404, "Not found or something went wrong!");
-    }
-
-    res.json({
-      status: "success",
-      code: 200,
-      videos: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-const getUsers = async (req, res, next) => {
-  try {
-    const result = await User.countDocuments();
-    if (!result) {
-      throw HttpError(404, "Not found or something went wrong!");
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      users: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-const getTraining = async (req, res, next) => {
-  try {
-    const result = await ExerciseLog.countDocuments();
-    if (!result) {
-      throw HttpError(404, "Not found or something went wrong!");
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      hours: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-const getCalories = async (req, res, next) => {
-  try {
+    const users = await User.countDocuments();
+    const exercises = await ExerciseLog.countDocuments();
     const array = await ExerciseLog.find();
     let count = 0;
-    if (!array) {
-      throw HttpError(404, "Not found or something went wrong!");
-    }
-    count = array.reduce((acc, item) => acc + item.calories, 0);
-
-    res.json({
-      status: "success",
-      code: 200,
-      calories: count,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-const getTime = async (req, res, next) => {
-  try {
-    const array = await ExerciseLog.find();
-    let count = 0;
-    if (!array) {
-      throw HttpError(404, "Not found or something went wrong!");
-    }
+    let calories = 0;
+    calories = array.reduce((acc, item) => acc + item.calories, 0);
     count = array.reduce((acc, item) => acc + item.time, 0);
 
     const finishCount = count / 60;
 
+    if (!videos || !users || !exercises || !array) {
+      throw HttpError(404, "Not found or something went wrong!");
+    }
+
     res.json({
       status: "success",
       code: 200,
-      hours: finishCount,
+      videos: videos,
+      numberOfRegistredUsers: users,
+      numberOfDoneTraining: exercises,
+      caloriesBurned: calories,
+      hoursExercise: finishCount,
     });
   } catch (error) {
     next(error);
   }
 };
+// const getUsers = async (req, res, next) => {
+//   try {
+//     const result = await User.countDocuments();
+//     if (!result) {
+//       throw HttpError(404, "Not found or something went wrong!");
+//     }
+//     res.json({
+//       status: "success",
+//       code: 200,
+//       users: result,
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+// const getTraining = async (req, res, next) => {
+//   try {
+//     const result = await ExerciseLog.countDocuments();
+//     if (!result) {
+//       throw HttpError(404, "Not found or something went wrong!");
+//     }
+//     res.json({
+//       status: "success",
+//       code: 200,
+//       hours: result,
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+// const getCalories = async (req, res, next) => {
+//   try {
+//     const array = await ExerciseLog.find();
+//     let count = 0;
+//     if (!array) {
+//       throw HttpError(404, "Not found or something went wrong!");
+//     }
+//     count = array.reduce((acc, item) => acc + item.calories, 0);
+
+//     res.json({
+//       status: "success",
+//       code: 200,
+//       calories: count,
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+// const getTime = async (req, res, next) => {
+//   try {
+//     const array = await ExerciseLog.find();
+//     let count = 0;
+//     if (!array) {
+//       throw HttpError(404, "Not found or something went wrong!");
+//     }
+//     count = array.reduce((acc, item) => acc + item.time, 0);
+
+//     const finishCount = count / 60;
+
+//     res.json({
+//       status: "success",
+//       code: 200,
+//       hours: finishCount,
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
 module.exports = {
-  getVideo,
-  getCalories,
-  getUsers,
-  getTime,
-  getTraining,
+  getStatistic,
 };
--- routes/statisticRouters.js
+++ routes/statisticRouters.js
@@ -2,10 +2,6 @@ const express = require("express");
 const router = express.Router();
 const statisticController = require("./controlers/statisticController");
 
-router.get("/video", statisticController.getVideo);
-router.get("/calories", statisticController.getCalories);
-router.get("/users", statisticController.getUsers);
-router.get("/time", statisticController.getTime);
-router.get("/training", statisticController.getTraining);
+router.get("/", statisticController.getStatistic);
 
 module.exports = router;
